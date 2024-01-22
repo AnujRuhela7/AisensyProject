973b4988ea6776a1780d9734b70f395575f6d440
AnujRuhela7 <ruhela7777@gmail.com>
2024-01-23 00:28:18 +0530

    Added Pagination Feature

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+if (process.env.NODE_ENV != "prodcution") require("dotenv").config();
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const app = express();
 
-const PORT = 3000;
-
 mongoose.
-  connect("mongodb+srv://ruhela7777:Vdo5zzz1NY7jRTlm@cluster0.dolftvr.mongodb.net/")
+  connect(process.env.DB_URL)
   .then(() => {
     console.log("DB Connected");
   })
@@ -29,7 +30,6 @@ app.use(express.static("public"));
 app.use(contactListRouter);
 app.use(importRouter);
 
-
-app.listen(PORT, () => {
-  console.log("Server is running at port " + PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server is running at port " + process.env.PORT);
 });
